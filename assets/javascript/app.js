c81c54c69b137864964980ed572a5fd0855102a0
Joe Mankin <joseaphmankin@gmail.com>
2018-09-19 09:37:52 -0500

    AJAX for Open Crime Data working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-
+//GOOGLE MAPS API
 //line to create a "New Map". Requires that you pass the DOM, 
 // A "Center" in Lat and Lng, and a zoom level. 
 
@@ -15,10 +15,68 @@ function initMap() {
         map: map,
         title: "Bat Bar"
     });
+    
+    marker.setMap(map)
 }
 
-marker.setMap(map)
 
 
+//CRIMESPOT API
+
+// var request = require('request');
+
+// var baseUrl = "http://api.spotcrime.com/crimes.json";
+// var key = "privatekeyforspotcrimepublicusers-commercialuse-877.410.1607";
+
+// module.exports = function(loc, radius, cb) {
+//   if (typeof radius === 'function') {
+//     cb = radius;
+//     radius = 0.01;
+//   }
+
+//   var rOpt = {
+//     url: baseUrl,
+//     json: true,
+//     qs: {
+//       lat: loc.lat,
+//       lon: loc.lon,
+//       key: key,
+//       radius: radius
+//     }
+//   };
+
+//   request(rOpt, function(err, res, body) {
+//     if (err) return cb(err);
+//     if (!body) return cb(new Error("No response"));
+//     cb(null, body.crimes);
+//   });
+
+// };
+
+// $.ajax({
+//     type: "get",
+//     url: "http://api.spotcrime.com/crimes.json", 
+//     dataType: "jsonp", 
+//     contentType: "application/json", 
+//     success: function(data){console.log(data)}
+// })
+
+let crimeLocation = "lat=41.8986&lon=-87.6628"
+let queryURL = "http://opendata.mybluemix.net/crimes?" + crimeLocation + "&radius=200";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    for(i = 0; i < 10; i++){
+    console.log(response.features[i]);
+    
+    console.log(response.features[i].geometry.coordinates);
+    $(".crime").append(` ${response.features[i].geometry.coordinates} + ${response.features[i].properties.desc} <br>`)
+    console.log(response.features[i].properties.desc);
+    // $(".crime").append(response.features[i].properties.desc)
+    };
+    
+  });
 
 
