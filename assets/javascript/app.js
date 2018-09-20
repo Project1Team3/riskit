1143eb1b63831fccf45a8760663a59f823c94eff
Joe Mankin <joseaphmankin@gmail.com>
2018-09-19 21:23:13 -0500

    Map on new Index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,39 @@
-    //GOOGLE MAPS API
-    //line to create a "New Map". Requires that you pass the DOM, 
-    // A "Center" in Lat and Lng, and a zoom level.     
+//GOOGLE MAPS API
+//line to create a "New Map". Requires that you pass the DOM, 
+// A "Center" in Lat and Lng, and a zoom level.     
 
 var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 41.852461, lng: -87.6409744 },
-            zoom: 17
-        });
-     
-    }
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 41.852461, lng: -87.6409744 },
+        zoom: 17
+    });
 
-    function getLatLngZip(zipcode) {
-        var geocoder = new google.maps.Geocoder();
-                var address = zipcode;
-                geocoder.geocode({ 'address': address }, function (results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        var latitude = results[0].geometry.location.lat();
-                        var longitude = results[0].geometry.location.lng();
-                        alert("Latitude: " + latitude + "\nLongitude: " + longitude);
-                    } else {
-                        alert("Request failed.")
-                    }
-                });
-                console.log([latitude, longitude]);
-                return [latitude, longitude];
-        }
+}
 
-      
+// function getLatLngZip(zipcode) {
+//     var geocoder = new google.maps.Geocoder();
+//             var address = zipcode;
+//             geocoder.geocode({ 'address': address }, function (results, status) {
+//                 if (status == google.maps.GeocoderStatus.OK) {
+//                     var latitude = results[0].geometry.location.lat();
+//                     var longitude = results[0].geometry.location.lng();
+//                     alert("Latitude: " + latitude + "\nLongitude: " + longitude);
+//                 } else {
+//                     alert("Request failed.")
+//                 }
+//             });
+//             console.log([latitude, longitude]);
+//             return [latitude, longitude];
+//     }
+
+
 
 $(document).ready(function () {
 
-    getLatLngZip(60026);
+    // getLatLngZip(60026);
 
-   
+
     let crimeLocation = "lat=41.852461&lon=-87.6409744"
     let queryURL = "http://opendata.mybluemix.net/crimes?" + crimeLocation + "&radius=500";
     $.ajax({
@@ -41,7 +41,7 @@ $(document).ready(function () {
         method: "GET"
     }).then(function (response) {
 
-        for (i = 0; i < 50; i++) {
+        for (i = 0; i < 30; i++) {
             console.log(response.features[i]);
 
             console.log(response.features[i].geometry.coordinates[0]);
@@ -51,14 +51,14 @@ $(document).ready(function () {
             // $(".crime").append(response.features[i].properties.desc)
 
             var marker = new google.maps.Marker({
-                position: { lat: response.features[i].geometry.coordinates[1], lng: response.features[i].geometry.coordinates[0]},
+                position: { lat: response.features[i].geometry.coordinates[1], lng: response.features[i].geometry.coordinates[0] },
                 map: map,
                 title: response.features[i].properties.desc,
                 animation: google.maps.Animation.DROP,
-               // icon: "assets/images/ski-mask.png",
+                // icon: "assets/images/ski-mask.png",
                 draggable: true
             });
-        
+
             marker.setMap(map)
 
 
@@ -66,7 +66,7 @@ $(document).ready(function () {
 
     });
 
- 
-    
+
+
 
 });
