a654c285ef701b141d660435a8736aa66726ec08
Joe Mankin <joseaphmankin@gmail.com>
2018-09-22 11:32:53 -0500

    Zip Button Tinker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ function initialize() {
     var center = new google.maps.LatLng(41.8882457, -87.6310917);
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
-        zoom: 17
+        zoom: 18
     });
 
     request = {
@@ -72,13 +72,13 @@ google.maps.event.addDomListener(window, 'load', initialize);
 //OPEN CRIME API w/ AJAX Call.
 
 let crimeLocation = "lat=41.8882457&lon=-87.6310917"
-let queryURL = "http://opendata.mybluemix.net/crimes?" + crimeLocation + "&radius=500";
+let queryURL = "http://opendata.mybluemix.net/crimes?" + crimeLocation + "&radius=200";
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
 
-    for (i = 0; i < 25; i++) {
+    for (i = 0; i < 20; i++) {
         console.log(response.features[i]);
 
         console.log(response.features[i].geometry.coordinates[0]);
@@ -98,7 +98,39 @@ $.ajax({
 
         marker.setMap(map)
 
-
     };
 
 });
+
+//On Click Function of ZIP Button via zip-btn class.
+
+$(document).on("click", ".zip-btn", function (event) {
+    var zipClicked = $(".zip-input").val().trim();   
+    console.log(zipClicked); 
+
+    // searchzip(zipClicked);
+})
+
+
+
+//Function that takes above data, makes an AJAX call to API, then takes the response and prepends as 
+// a Bootstrap card. 
+
+// const searchzip = zip => {
+//     $.get("https://api.giphy.com/v1/zips/search?q=" + zip + "&api_key=dc6zaTOxFJmzC").then(data => {
+
+//         for (let i = 0; i < 10; i++) {
+//             $(".zipWindow").prepend(`
+
+//             <div class="card" style="width: 25rem;">
+//                 <img class="card-img-top still" src="${data.data[i].images.fixed_height_still.url}" data-still='${data.data[i].images.fixed_height_still.url}' data-original='${data.data[i].images.original.url}' height=300 />
+//                     <div class="card-body moving">
+//                         <p class="card-text">Rated: ${data.data[i].rating}</p>
+//                      </div>
+//             </div>
+
+                
+//             `)
+//         }
+//     });
+// }
