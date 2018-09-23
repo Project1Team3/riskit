64797b1d205f5e12052a8929e287c4895f6cd120
Joe Mankin <joseaphmankin@gmail.com>
2018-09-23 11:49:28 -0500

    Firebase Card Added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
     })
 })
 
-
 var map;
 var infowindow = new google.maps.InfoWindow();
 
@@ -16,9 +15,18 @@ var request;
 var service;
 var markers = [];
 
+//VARs for all the crime icons
+let robberyIcon = "assets/images/robbery.png";
+let theftIcon = "assets/images/theft.png";
+let shootingIcon = "assets/images/shooting.png";
+let crimeIcon = "assets/images/crimescene.png";
+let bombIcon = "assets/images/bomb.png";
+let skullIcon = "assets/images/pirates.png"
+
+
 
 function initialize() {
-    var center = new google.maps.LatLng(41.8809195,-87.6276138);
+    var center = new google.maps.LatLng(41.8817767, -87.6393348);
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
         zoom: 17
@@ -29,7 +37,6 @@ function initialize() {
         radius: 30000,
         types: ['bar', 'restaurant', 'club', 'pub']
     };
-
 
     service = new google.maps.places.PlacesService(map);
 
@@ -71,6 +78,8 @@ function createMarker(place) {
 google.maps.event.addDomListener(window, 'load', initialize)
 
 
+//ZIP CODE BUTTON RECENTER AND AJAX CALL
+
 $(".zipbutton").on("click", function zip(event) {
     var zip = $(".zipinput").val().trim()
     let zipURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zip + "&key=AIzaSyAEbmR9tGPSpWie_laz4e2EBDOIdgGp_gE"
@@ -90,7 +99,7 @@ $(".zipbutton").on("click", function zip(event) {
         console.log(lng)
         //var lat = 41.8986
         //var lng = -87.6628
-        map.setCenter(new google.maps.LatLng(`${lat}`,`${lng}`));
+        map.setCenter(new google.maps.LatLng(`${lat}`, `${lng}`));
         var crimeLocation = `lat=${lat}&lon=${lng}`;
         var queryURL = `http://opendata.mybluemix.net/crimes?${crimeLocation}&radius=500`;
         marker(queryURL)
@@ -118,7 +127,7 @@ function marker(queryURL) {
                 map: map,
                 title: response.features[i].properties.desc,
                 animation: google.maps.Animation.DROP,
-                icon: "assets/images/robbery.png",
+                icon: crimeIcons(response.features[i].properties.type),
                 draggable: true
             })
 
@@ -126,4 +135,122 @@ function marker(queryURL) {
         }
     })
 }
-//});
+
+//INITIAL OPEN CRIME API w/ AJAX Call. 
+
+let crimeLocation = "lat=41.8817767&lon=-87.6393348";
+let queryURL = "http://opendata.mybluemix.net/crimes?" + crimeLocation + "&radius=500";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+
+    for (i = 0; i < 10; i++) {
+        console.log(response.features[i].properties.type);
+        console.log(response.features[i]);
+        console.log(response.features[i].geometry.coordinates[0]);
+        console.log(response.features[i].geometry.coordinates[1]);
+        $(".crime").append(` ${response.features[i].geometry.coordinates}, ${response.features[i].properties.desc},  ${response.features[i].properties.type} <br>`)
+        console.log(response.features[i].properties.desc);
+        // $(".crime").append(response.features[i].properties.desc)
+
+        var marker = new google.maps.Marker({
+            position: { lat: response.features[i].geometry.coordinates[1], lng: response.features[i].geometry.coordinates[0] },
+            map: map,
+            title: response.features[i].properties.desc,
+            animation: google.maps.Animation.DROP,
+            icon: crimeIcons(response.features[i].properties.type),
+            draggable: true
+        });
+
+        marker.setMap(map)
+
+
+    };
+});
+
+//Switch statement for assigning crime icons to Ajax call codes
+function crimeIcons(crimeID) {
+
+    switch (crimeID) {
+        case "03":
+            return shootingIcon;
+            break;
+        case "06":
+            return theftIcon;
+            break;
+        case "08B":
+            return robberyIcon;
+            break;
+        case "26":
+            return skullIcon;
+            break;
+        default:
+            return crimeIcon;
+    }
+}
+
+//Comment Cards connected to FireBase
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA94oLjra38WWILGLTlxi1sMY02qZHAHCE",
+    authDomain: "riskit-1537281695115.firebaseapp.com",
+    databaseURL: "https://riskit-1537281695115.firebaseio.com",
+    projectId: "riskit-1537281695115",
+    storageBucket: "riskit-1537281695115.appspot.com",
+    messagingSenderId: "331530995468"
+  };
+  firebase.initializeApp(config)
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+// Capture Button Click
+$("#add-user").on("click", function (event) {
+    // prevent page from refreshing when form tries to submit itself
+    event.preventDefault();
+
+    // Capture user inputs and store them into variables
+    var name = $("#name-input").val().trim();
+    var place = $("#place-input").val().trim();
+    var food = $("#food-input").val().trim();
+    var danger = $("#danger-input").val().trim();
+    var comment = $("#comment-input").val().trim();
+
+
+    // Code for handling the push
+    database.ref("/users").push({
+        name: name,
+        place: place,
+        food: food,
+        danger: danger,
+        comment: comment,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+});
+
+// Firebase watcher .on("child_added"
+database.ref("/users").on("child_added", function (snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Change the HTML to reflect
+    $("#name-display").text(sv.name);
+    $("#place-display").text(sv.place);
+    $("#food-display").text(sv.food);
+    $("#danger-display").text(sv.danger);
+    $("#comment-display").text(sv.comment);
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+// By default display the content from localStorage
+$("#name-display").text(localStorage.getItem("name"));
+$("#place-display").text(localStorage.getItem("place"));
+$("#food-display").text(localStorage.getItem("food"));
+$("#danger-display").text(localStorage.getItem("danger"));
+$("#comment-display").text(localStorage.getItem("comment"));
